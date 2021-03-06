47a0ba78ce762a012358d2fab957e3a101b0fcea
Tomas <tomaswegenast@gmail.com>
2021-03-06 19:36:35 -0300

    Update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,3 @@
-## [1.0.0] - Initial version
+## [1.0.7] - Methods converted to static
 ## [1.0.1] - Fix bug
+## [1.0.0] - Initial version
--- lib/src/logger/base/logger.dart
+++ lib/src/logger/base/logger.dart
@@ -29,39 +29,43 @@ class Logger {
   LogLevel get minimumLevel => _level;
 
   /// Logs an [LogLevel.Info] message
-  void info(String message, {dynamic error, StackTrace stackTrace, Map<String, dynamic> data}) {
+  static void info(String message, {dynamic error, StackTrace stackTrace, Map<String, dynamic> data}) {
     _log(LogLevel.Info, message, data, error, stackTrace);
   }
 
   /// Logs an [LogLevel.Debug] message
-  void debug(String message, {dynamic error, StackTrace stackTrace, Map<String, dynamic> data}) {
+  static void debug(String message, {dynamic error, StackTrace stackTrace, Map<String, dynamic> data}) {
     _log(LogLevel.Debug, message, data, error, stackTrace);
   }
 
   /// Logs an [LogLevel.Warning] message
-  void warning(String message, {dynamic error, StackTrace stackTrace, Map<String, dynamic> data}) {
+  static void warning(String message, {dynamic error, StackTrace stackTrace, Map<String, dynamic> data}) {
     _log(LogLevel.Warning, message, data, error, stackTrace);
   }
 
   /// Logs an [LogLevel.Fatal] message
-  void fatal(String message, {dynamic error, StackTrace stackTrace, Map<String, dynamic> data}) {
+  static void fatal(String message, {dynamic error, StackTrace stackTrace, Map<String, dynamic> data}) {
     _log(LogLevel.Fatal, message, data, error, stackTrace);
   }
 
   /// Logs an [LogLevel.Error] message
-  void error(String message, {dynamic error, StackTrace stackTrace, Map<String, dynamic> data}) {
+  static void error(String message, {dynamic error, StackTrace stackTrace, Map<String, dynamic> data}) {
     _log(LogLevel.Error, message, data, error, stackTrace);
   }
 
-  void _log(LogLevel level, String message, Map<String, dynamic> data, dynamic error, StackTrace stackTrace) {
-    if(level.index < minimumLevel.index) {
+  static void _log(LogLevel level, String message, Map<String, dynamic> data, dynamic error, StackTrace stackTrace) {
+    if(_instance == null) {
+      throw new Exception("Logger not initialized yet. Use Logger().");
+    }
+
+    if(level.index < _instance.minimumLevel.index) {
       return;
     }
 
     LogEvent event = LogEvent(level, message: message, data: data, error: error, stackTrace: stackTrace);
-    if(_logFilter.shouldLog(event)) {
+    if(_instance._logFilter.shouldLog(event)) {
       try {
-        _logOutput.write(event);
+        _instance._logOutput.write(event);
       } catch(err) {
         throw err;
       }
